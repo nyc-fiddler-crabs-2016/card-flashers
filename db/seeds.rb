c984dee9da2238e7e1730f30cb05fe2b6290f891
Akeem Street & gary tso <pair+akeem-s.superboyblue@devbootcamp.com>
2016-03-04 15:58:33 -0500

    Add faker seeds for database

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+require 'faker'
+
+5.times do
+  User.create(
+    user_name: Faker::Hacker.noun,
+    password: "password"
+    )
+end
+
+10.times do
+  Deck.create(
+    name: Faker::Hacker.noun,
+    description: Faker::Hacker.say_something_smart
+
+    )
+end
+
+
+50.times do
+  Card.create(
+    deck_id: Deck.all.sample.id
+    )
+end
+
+
+
+
+
+
