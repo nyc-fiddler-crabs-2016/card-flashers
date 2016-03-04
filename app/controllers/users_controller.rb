4b8826fd86229db06c1ecbc7eb49d5631655385c
Peter & Tim Beck <pair+petey101.timbek2@devbootcamp.com>
2016-03-04 16:48:29 -0500

    create MVP login page with password error

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+get '/users' do
+  "hello"
+end
+
+get '/login' do
+  erb :'/users/login'
+end
+
+post '/login' do
+  if @user = User.find_by(user_name:params[:user_name]) && params[:password] == @user.password
+      redirect "/users/#{@user.id}"
+    else
+      @errors = "Thats wrong stupid"
+      erb :'/users/login'
+    end
+end
